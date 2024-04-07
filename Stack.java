40ddc2d846afa883804f378d926e7a907f2bc4c3
TotalCranberry <147934022+TotalCranberry@users.noreply.github.com>
2024-04-07 18:17:27 +0530

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,41 @@
+public class Stack{
+  Node top;
+
+  public Stack(int data){
+    this.top = null;
+  }
+  
+  public boolean isEmpty(){
+    if(this.top==null){
+      return true;
+    }
+    return false;
+  }
+
+  public void push(int data){
+    Node newNode = new Node(data);
+    if(!isEmpty){
+      newNode.next = this.top; 
+    }
+    this.top = newNode;
+  }
+
+  public int pop(){
+    if(!isEmpty){
+      Node temp = this.top;
+      this.top = this.top.next;
+      int data = temp.data;
+      temp = null;
+      return data;
+    }
+    return 0;
+  }
+
+  public void display(){
+    Node current = this.top;
+    while(current!=null){
+      System.out.print(current.data+" ");
+      current = current.next;
+    }
+  }
+}
